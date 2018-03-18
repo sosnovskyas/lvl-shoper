b3ed1807ff4c7fa02bc88377256c0ff4545b4f04
sosnovskyas <sosnovsky.as@gmail.com>
2018-03-18 14:04:17 +0300

    created index logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,18 @@
-console.log('ok');
+import * as React from 'react';
+import {render} from 'react-dom';
+import {Home} from './containers/home';
+import {firebaseSaga} from './modules/firebase/firebase-saga';
+import {sagaMiddleware, store} from './modules/store/store-module';
+import {Provider} from 'react-redux';
+
+export const root = document.querySelector('#lvl-shoper');
+
+
+render(
+  <Provider store={store}>
+    <Home/>
+  </Provider>,
+  root
+);
+
+sagaMiddleware.run(firebaseSaga);
