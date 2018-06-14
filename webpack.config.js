a6cc7d1275607a2966ba604801776060ec30e718
sosnovskyas <sosnovsky.as@gmail.com>
2018-06-15 01:33:43 +0300

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ManifestPlugin = require('webpack-manifest-plugin');
+const ManifestPlugin = require("webpack-manifest-plugin");
 
 //noinspection JSUnresolvedVariable
 module.exports = {
-    entry: path.join(__dirname, 'src', 'index.tsx'),
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ]
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html'
-        }),
-        new ManifestPlugin({
-            filename: 'manifest.json'
-        })
+  entry: path.join(__dirname, "src", "index.tsx"),
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
     ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new ManifestPlugin({
+      filename: "manifest.json"
+    })
+  ],
+  devServer: {
+    // hot: true,
+    // hotOnly: true,
+    // host: "0.0.0.0",
+    historyApiFallback: true
+  }
 };
